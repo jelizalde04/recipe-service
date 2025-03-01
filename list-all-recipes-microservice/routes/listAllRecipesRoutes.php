69f100e42426deb9438f6f7389fa8f1d95265a00
Alexis Mendoza <afmendozaf@uce.edu.ec>
2025-03-01 16:23:19 -0500

    feat(routes): 🔀 add routes for listing all recipes

---
+++ list-all-recipes-microservice/routes/listAllRecipesRoutes.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'controllers/ListAllRecipesController.php';
+
+use Psr\Http\Message\ServerRequestInterface;
+
+function handleRequest(ServerRequestInterface $request)
+{
+    $path = $request->getUri()->getPath();
+    $method = $request->getMethod();
+
+    if ($method === 'GET' && $path === '/list-all-recipes') {
+        return (new ListAllRecipesController())->listAllRecipes();
+    }
+
+    return new React\Http\Message\Response(
+        404,
+        ['Content-Type' => 'application/json'],
+        json_encode(['error' => 'Route not found'])
+    );
+}
